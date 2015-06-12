6094cc078ad4927b4ece04212439da0decda4b01
Yo Wakita <wakita@gmail.com>
2015-06-12 15:54:40 -0700

    Adds statics controller and fixes route.

---
+++ app/controllers/statics_controller.rb
@@ -0,0 +1,10 @@
+class StaticsController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  root 'statics#home'
+
   resources :users
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
