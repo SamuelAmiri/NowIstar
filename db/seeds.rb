2e926f8e3ef785e93fa2d2d989d6abafd51a07c8
Yo Wakita <wakita@gmail.com>
2015-06-16 13:18:07 -0700

    Adds seed data for 6 of 17 categories.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-categories = Category.create([
+categories = Category.create
+	([
 	{name: 'Art & Creative'},
 	{name: 'Automotive & Bike'},
 	{name: 'Beauty'},
@@ -25,3 +26,47 @@ categories = Category.create([
 	{name: 'Real Estate'},
 	{name: 'Travel & Vacation'},
 	])
+
+subcategories = Subcategory.create
+	([
+	{category_id: 1 , name: 'Dance' },
+	{category_id: 1 , name: 'Paint' },
+	{category_id: 1 , name: 'Performance' },
+	{category_id: 1 , name: 'Photography' },
+	{category_id: 1 , name: 'Modeling' },
+	{category_id: 1 , name: 'Tattoo' },
+	{category_id: 1 , name: 'Videography' },
+
+	{category_id: 2 , name: 'Auto Repair' },
+	{category_id: 2 , name: 'Body Work & Paint' },
+	{category_id: 2 , name: 'Car Wash & Detailing' },
+	{category_id: 2 , name: 'Stereos & Speakers' }
+	{category_id: 2 , name: 'Tint & Window Repair' },
+	{category_id: 2 , name: '' },
+
+	{category_id: 3 , name: 'Make Up' },
+	{category_id: 3 , name: 'Manicure & Pedicure' },
+	{category_id: 3 , name: 'Hair Styling' },
+	{category_id: 3 , name: 'Esthetician' },
+	{category_id: 3 , name: 'Wardrobe Styling' },
+	{category_id: 3 , name: 'Facials & Waxing' },
+
+	{category_id: 4 , name: 'Accounting & Taxes' }
+	{category_id: 4 , name: 'Business Advisory & Consulting' }
+	{category_id: 4 , name: 'Recruiting' }
+	{category_id: 4 , name: 'Financial Services & Planning' }
+	{category_id: 4 , name: 'Insurance' }
+
+	{category_id: 5 , name: 'Child Care' }
+	{category_id: 5 , name: 'Eldery Care' }
+	{category_id: 5 , name: 'Pet Care & Boarding' }
+	{category_id: 5 , name: 'House Sitting' }
+
+	{category_id: 6 , name: 'Private Tutoring' }
+	{category_id: 6 , name: 'Music Lessons' }
+	{category_id: 6 , name: 'Acting Instruction' }
+	{category_id: 6 , name: 'Art Instruction' }
+	{category_id: 6 , name: 'Language Instruction' }
+	])
+
+
