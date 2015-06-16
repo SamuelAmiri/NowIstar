7fffcb9391521f7ce40ece11feaaa87860510818
Yo Wakita <wakita@gmail.com>
2015-06-16 16:01:54 -0700

    Corrects reference to references in migration file and model

--- db/migrate/20150616230126_create_skills.rb
+++ db/migrate/20150616230126_create_skills.rb
@@ -1,8 +1,8 @@
 class CreateSkills < ActiveRecord::Migration
   def change
     create_table :skills do |t|
-      t.reference :user
-      t.reference :subcategory
+      t.references :user, index: true, foreign_key: true
+      t.references :subcategory, index: true, foreign_key: true
       t.decimal :price
       t.text :description
       t.string :image
