a100f01fe00106abd2873d3e0cc0e82bc77c6607
Samuel Amiri <samuelamiri@gmail.com>
2015-06-17 16:11:47 -0700

    Update to user model to extrat email from providers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ActiveRecord::Base
 	    user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
 	    user.fname = auth_hash['info']['first_name']
 	    user.lname = auth_hash['info']['last_name']
-	    #user.image = auth_hash['info']['image']
+	    user.image = auth_hash['info']['image']
+	    user.email = auth_hash['info']['email']
 	    user.save!
 	    user
 	  end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,11 +2,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
        provider :developer unless Rails.env.production?
        provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"],
        	scope: 'public_profile', 
-       	info_fields: 'id,first_name,last_name, image'
+       	info_fields: 'id,first_name,last_name, email, image'
        	# Adds support for linkedin with key
        provider :linkedin, ENV['linkedin_client_id'], ENV['linkedin_secret'],
-       	scope: 'r_basicprofile',
-  		fields: ['id', 'first-name', 'last-name', 'picture-url']
+        scope:  [ 'r_basicprofile', 'r_emailaddress'],
+  		  fields: ['id', 'first-name', 'last-name', 'picture-url', 'email-address']
 
   		OmniAuth.config.on_failure = Proc.new do |env|
 		  SessionsController.action(:auth_failure).call(env)
