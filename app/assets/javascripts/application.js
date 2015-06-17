255ea796c5f1e7c62a2f4eb8312fc2dddc5e4691
Yo Wakita <wakita@gmail.com>
2015-06-16 19:53:13 -0700

    Adds google map and styling of google map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 //= require turbolinks
 //= require_tree .
 
+function initialize(){
+	var mapProperties = {
+  		center:new google.maps.LatLng(34.0129821,-118.495196), zoom: 17,mapTypeId: google.maps.MapTypeId.ROADMAP
+  	};
+  	var map=new google.maps.Map(document.getElementById("googleMap"), mapProperties);
+		
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
