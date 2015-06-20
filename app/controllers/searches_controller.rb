0eaed505f662825a845f35e899fbd4038cfe8b2c
Yo Wakita <wakita@gmail.com>
2015-06-20 13:45:35 -0700

    Adds comments to searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,22 @@
 class SearchesController < ApplicationController
+	## Pry pauses the code where 'binding pry' is inserted into the code.
+	## Good for testing.
 	require 'pry'
-	def search
 
+	def search
+	## :type refers to the param in a hidden text input field in the search partial.
+	## :id refers to the param in a hidden text input field in the search partial.
+	## :location refers to the input in the text field in the search partial.
+	## .near is a method that is defined by the geocoder gem, giving the ability
+	## to search via proximity to the location inputted into the search field.
 		if params[:type] == "subcategory"
       		skills = Skill.where(subcategory_id: params[:id])
       		location = (params[:location])
       		@skills = skills.near(location, 30)
       		@skills
-      		# @skills = skills.near(location, 20)
+
+    ## Due to skills not being directed associated to categories, an empty array is
+    ##  created and is shoveled skills that are filtered.
     	elsif params[:type] == "category"
 			@subcategories = Subcategory.where(category_id: params[:id])
 			@skills = []
