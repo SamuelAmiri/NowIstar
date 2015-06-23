3769a678f4a2d5cc5c543fb4383216af454d4038
Andre Trelles <andretrelles@hotmail.com>
2015-06-23 10:28:22 -0700

    validates name uniqueness

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -12,11 +12,10 @@ it "Prevents a new category from being made if name is not entered" do
 	expect(category).to be_valid
 end
 
-it "Reqiores a new category to be unique" do
-	category = Category.new(name: nil)
-	expect(category).to be_invalid
+it "Reqiures a new category to be unique" do
+	category = Category.create(name: "Beauty")
+	category2 = Category.create(name: "Beauty")
+	expect(category2).to be_invalid
 end
-
-
 
 end
