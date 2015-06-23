baf7656d62a538bafbb21cee0c59bb65e0cdacb8
Andre Trelles <andretrelles@hotmail.com>
2015-06-23 09:57:11 -0700

    tests for a new category to be unique

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+it "Creates a new category" do
+	category = Category.new(name: "abc")
+	expect(category).to be_valid
+end
+
+it "Prevents a new category from being made if name is not entered" do
+	category = Category.new(name: "Andre")
+	expect(category).to be_valid
+end
+
+it "Reqiores a new category to be unique" do
+	category = Category.new(name: nil)
+	expect(category).to be_invalid
+end
+
+
+
 end
