8f6c099e73cff250cd448f3e74c940eecb4ef866
Samuel Amiri <samuelamiri@gmail.com>
2015-06-25 20:16:43 -0700

    updated user view with sales page

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   
 
-  get 'users/:id/sales' => "charges#sales"
+  get 'users/:id/sales' => "charges#sales", as: "sales"
   get 'users/:id/purchases' => "charges#purchases"
 
   resources :categories
