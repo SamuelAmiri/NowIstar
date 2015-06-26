4e2b631fbbf632cf478497c3999e7d44d41ec689
Andre Trelles <andretrelles@hotmail.com>
2015-06-26 12:12:34 -0700

    changes new skill fontsand position

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626030551) do
+ActiveRecord::Schema.define(version: 20150626172330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,11 +78,11 @@ ActiveRecord::Schema.define(version: 20150626030551) do
   create_table "skills", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "subcategory_id"
-    t.decimal  "price",          precision: 6, scale: 2
+    t.decimal  "price"
     t.text     "description"
     t.string   "image"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "title"
     t.string   "address"
     t.string   "city"
