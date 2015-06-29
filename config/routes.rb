33772fb56093a0b8cf87412805ab86d3c3fad766
Yo Wakita <wakita@gmail.com>
2015-06-28 20:18:44 -0700

    Adds testing static page for landing

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   root 'statics#home'
 
+  get 'statics/about' => "statics#about"
+
   resources :users do
     resources :skills, except: :index do
      resources :charges, only: [:new, :create]
@@ -33,7 +35,6 @@ Rails.application.routes.draw do
 
   resources :categories
   resources :subcategories
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
